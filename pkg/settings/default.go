6b6755eab4cbbff5499c94af7daf3fdc3cad9b3d
Guangbo Chen <steven.guangbo.chen@gmail.com>
2024-12-16 23:01:23 +0800

    fix: update notebook images & fix mini bugs

--- pkg/settings/default.go
+++ pkg/settings/default.go
@@ -2,6 +2,7 @@ package settings
 
 import (
 	"encoding/json"
+	"fmt"
 
 	"github.com/sirupsen/logrus"
 )
@@ -12,7 +13,10 @@ type Image struct {
 	Default        bool   `json:"default,omitempty"`
 }
 
-const defaultImgVersion = "v0.1.0"
+const (
+	defaultImgRepo    = "ghcr.io/oneblock-ai"
+	defaultImgVersion = "v0.1.0"
+)
 
 // SetDefaultNotebookImages set default notebook images
 // resources please refer to: https://www.kubeflow.org/docs/components/notebooks/container-images/
@@ -20,57 +24,57 @@ func setDefaultNotebookImages() string {
 	defaultImgs := map[string][]Image{
 		"jupyter": {
 			{
-				ContainerImage: "oneblockai/jupyter-scipy:" + defaultImgVersion,
+				ContainerImage: fmt.Sprintf("%s/%s:%s", defaultImgRepo, "jupyter-scipy", defaultImgVersion),
 				Description:    "JupyterLab + PyTorch",
 				Default:        true,
 			},
 			{
-				ContainerImage: "oneblockai/jupyter-pytorch:" + defaultImgVersion,
+				ContainerImage: fmt.Sprintf("%s/%s:%s", defaultImgRepo, "jupyter-pytorch", defaultImgVersion),
 				Description:    "JupyterLab + PyTorch",
 			},
 			{
-				ContainerImage: "oneblockai/jupyter-pytorch-full:" + defaultImgVersion,
+				ContainerImage: fmt.Sprintf("%s/%s:%s", defaultImgRepo, "jupyter-pytorch-full", defaultImgVersion),
 				Description:    "JupyterLab + PyTorch + Common Packages",
 			},
 			{
-				ContainerImage: "oneblockai/jupyter-pytorch-cuda:" + defaultImgVersion,
+				ContainerImage: fmt.Sprintf("%s/%s:%s", defaultImgRepo, "jupyter-pytorch-cuda", defaultImgVersion),
 				Description:    "JupyterLab + PyTorch + CUDA",
 			},
 			{
-				ContainerImage: "oneblockai/jupyter-pytorch-cuda-full:" + defaultImgVersion,
+				ContainerImage: fmt.Sprintf("%s/%s:%s", defaultImgRepo, "jupyter-pytorch-cuda-full", defaultImgVersion),
 				Description:    "JupyterLab + PyTorch + CUDA + Common Packages",
 			},
 			{
-				ContainerImage: "oneblockai/jupyter-tensorflow:" + defaultImgVersion,
+				ContainerImage: fmt.Sprintf("%s/%s:%s", defaultImgRepo, "jupyter-tensorflow", defaultImgVersion),
 				Description:    "JupyterLab + PyTorch",
 			},
 			{
-				ContainerImage: "oneblockai/jupyter-tensorflow-full:" + defaultImgVersion,
+				ContainerImage: fmt.Sprintf("%s/%s:%s", defaultImgRepo, "jupyter-tensorflow-full", defaultImgVersion),
 				Description:    "JupyterLab + PyTorch + Common Packages",
 			},
 			{
-				ContainerImage: "oneblockai/jupyter-tensorflow-cuda:" + defaultImgVersion,
+				ContainerImage: fmt.Sprintf("%s/%s:%s", defaultImgRepo, "jupyter-tensorflow-cuda", defaultImgVersion),
 				Description:    "JupyterLab + PyTorch + CUDA",
 			},
 			{
-				ContainerImage: "oneblockai/jupyter-tensorflow-cuda-full:" + defaultImgVersion,
+				ContainerImage: fmt.Sprintf("%s/%s:%s", defaultImgRepo, "jupyter-tensorflow-cuda-full", defaultImgVersion),
 				Description:    "JupyterLab + PyTorch + CUDA + Common Packages",
 			},
 			{
-				ContainerImage: "oneblockai/jupyter-pipeline:" + defaultImgVersion,
+				ContainerImage: fmt.Sprintf("%s/%s:%s", defaultImgRepo, "jupyter-pipeline", defaultImgVersion),
 				Description:    "JupyterLab + Elyra Pipeline",
 			},
 		},
 		"code-server": {
 			{
-				ContainerImage: "oneblockai/codeserver-python:" + defaultImgVersion,
+				ContainerImage: fmt.Sprintf("%s/%s:%s", defaultImgRepo, "code-server-python", defaultImgVersion),
 				Description:    "Visual Studio Code + Conda Python",
 				Default:        true,
 			},
 		},
 		"rstudio": {
 			{
-				ContainerImage: "oneblockai/rstudio-tidyverse:" + defaultImgVersion,
+				ContainerImage: fmt.Sprintf("%s/%s:%s", defaultImgRepo, "rstudio-tidyverse", defaultImgVersion),
 				Description:    "RStudio + Tidyverse",
 				Default:        true,
 			},
